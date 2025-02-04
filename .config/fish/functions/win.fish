3b27115ab9deeaec72b75d259fcd6ebfa50903d4
Rafael Delgado Martins <rafael@delgadomartins.com>
2025-02-04 09:31:51 -0300

    pre fedora

--- .config/fish/functions/win.fish
+++ .config/fish/functions/win.fish
@@ -3,6 +3,6 @@ function win
 
   if test "$confirm" = "S" -o "$confirm" = "s" -o "$confirm" = "Y" -o "$confirm" = "y"
     sudo efibootmgr -n 0003 > /dev/null &&
-      qdbus-qt6 org.kde.Shutdown /Shutdown logoutAndReboot
+      qdbus6 org.kde.Shutdown /Shutdown logoutAndReboot
     end
 end
